2f2379ba9cd365096d8d6905f35c418fdaa2853d
Berindar8 <dashakuzmich8@gmail.com>
2022-11-03 00:23:14 +0300

    add form

---
+++ register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Регистрация</title>
+</head>
+<body>
+    <div class="regPage">
+        <div class="regPage__content">
+            <h1>Регистрация</h1>
+            <form>
+                <label>Логин</label>
+                <input type="text" name="login" id="login" placeholder="Введите логин"><br>
+                <label>Пароль</label>
+                <input type="password" name="pass" id="pass" placeholder="Введите пароль"><br>
+                <label>Подтверждение пароля</label>
+                <input type="password" name="pass_confirm" id="pass_confirm" placeholder="Подтвердите пароль"><br>
+                <label>Почта</label>
+                <input type="email" name="email" id="email" placeholder="Введите e-mail"><br>
+                <label>Имя</label>
+                <input type="text" name="username" id="username" placeholder="Введите имя"><br>
+                <button class="btn_reg" type="submit">Регистрация</button>
+                <p>
+                    У вас есть аккаунт? - <a href="index.html">Авторизируйтесь</a>!
+                </p>
+            </form> 
+        </div>
+    </div>    
+</body>
+</html>
+</body>
+</html>
