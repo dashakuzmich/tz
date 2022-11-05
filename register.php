8e03034ee6c0e9556472d6f6b188872dac034d7a
Berindar8 <dashakuzmich8@gmail.com>
2022-11-05 22:51:43 +0300

    change register.php

--- register.php
+++ register.php
@@ -8,26 +8,19 @@
     <title>Регистрация</title>
 </head>
 <body>
-    <div class="regPage">
-        <div class="regPage__content">
-            <h1>Регистрация</h1>
-            <form>
-                <label>Логин</label>
-                <input type="text" name="login" id="login" placeholder="Введите логин"><br>
-                <label>Пароль</label>
-                <input type="password" name="pass" id="pass" placeholder="Введите пароль"><br>
-                <label>Подтверждение пароля</label>
-                <input type="password" name="pass_confirm" id="pass_confirm" placeholder="Подтвердите пароль"><br>
-                <label>Почта</label>
-                <input type="email" name="email" id="email" placeholder="Введите e-mail"><br>
-                <label>Имя</label>
-                <input type="text" name="username" id="username" placeholder="Введите имя"><br>
-                <button class="btn_reg" type="submit">Регистрация</button>
-                <p>
-                    У вас есть аккаунт? - <a href="index.php">Авторизируйтесь</a>!
-                </p>
-            </form> 
-        </div>
+    <div class="page">
+        <h1>Регистрация</h1>
+        <form class="form">
+        <input type="text" name="login" id="login" placeholder="Логин"><br>
+        <input type="password" name="pass" id="pass" placeholder="Пароль"><br>
+        <input type="password" name="pass_confirm" id="pass_confirm" placeholder="Подтвердите пароль"><br>
+        <input type="email" name="email" id="email" placeholder="Почта"><br>
+        <input type="text" name="username" id="username" placeholder="Имя"><br>
+        <button class="btn_reg" type="submit">Регистрация</button>
+        <p>
+             У вас есть аккаунт? - <a href="index.php">Авторизируйтесь!</a>
+        </p>
+        </form> 
     </div>    
 </body>
 </html>
