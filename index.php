7d7042ff0f88ac0847f888e6815c411d7ea240d4
Berindar8 <dashakuzmich8@gmail.com>
2022-11-05 22:51:17 +0300

    change index.php

--- index.php
+++ index.php
@@ -4,23 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
     <title>Авторизация</title>
 </head>
 <body>
-    <div class="authPage">
-        <div class="authPage__content">
-            <h1>Авторизация</h1>
-            <form class="authForm">
-                <label>Логин</label>
-                <input type="text" name="login" id="login" placeholder="Введите логин"><br>
-                <label>Пароль</label>
-                <input type="password" name="pass" id="pass" placeholder="Введите пароль"><br>
-                <button class="btn_auth" type="submit">Войти</button>
-                <p>
-                     У вас нет аккаунта? - <a href="register.php">Зарегистрируйтесь!</a>
-                </p>
+    <div class="page">
+        <h1>Добро пожаловать!</h1>
+        <form>
+            <input type="text" name="login" id="login" placeholder="Логин"><br>
+            <input type="password" name="pass" id="pass" placeholder="Пароль"><br>
+            <button class="btn_auth" type="submit">Войти</button>
+            <p>
+                У вас нет аккаунта? - <a href="register.php">Зарегистрируйтесь!</a>
+            </p>
             </form>
-        </div>
     </div>
 </body>
 </html>
